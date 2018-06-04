0b558bdc8b9a7ee51414ba2dea1eceade7614c60
Andrew <markovandrew@yandex.ru>
2018-06-04 10:21:37 +0700

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,20 +1,24 @@
-let a = process.argv[2];
-let b = process.argv[3];
-let result = -1;
+var a = process.argv[2];
+var b = process.argv[3];
+var result = -1;
 
 if (a.length == b.length) {
-
-    for (var i = 1; i < a.length; i++) {
-        a = a.substring(1) + a[0];
-        if (a == b) {
-            if (a.length - i > i) {
-                result = i;
-            } else {
-                result = a.length - i;
+    if (a == b) {
+        result = 0;
+    } else {
+        for (var i = 1; i < a.length; i++) {
+            a = a.substring(1) + a[0];
+            if (a == b) {
+                if (a.length - i > i) {
+                    result = i;
+                } else {
+                    result = a.length - i;
+                }
+                break;
             }
-            break;
         }
     }
+    
 }
 
-console.log(result);
+process.stdout.write(result);
